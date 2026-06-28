bb152406a4cc84520524e32ee3799c86f3b3a867
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-28 17:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	if(str==null) return [];
+	let res=[];
+	for(let i=0;i<str.length;i=i+size){ // all substring of same length aecept the last one
+		res.push(str.slice(i,i+size));
+	}
+	return res;
 }
 
-// Do not change the code below
+// // Do not change the code below
 const str = prompt("Enter String.");
+
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
